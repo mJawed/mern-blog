15458cbe6e34c1702ed9762582b2a5f084f9cbf2
mJawed <mohd.jawed@alcanttor.com>
2023-12-29 22:29:42 +0530

    added header and signin paghes html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from "./dashboard/pages/SignIn";
 import SignUp from "./dashboard/pages/SignUp";
 import About from "./dashboard/pages/About";
+import HeaderDash from "./componets/HeaderDash";
 
 function App() {
   return ( 
    <BrowserRouter>
+   <HeaderDash/>
    <Routes>
+
 <  Route path="/dashboard/signin" element={<SignIn/>} />
 <  Route path="/dashboard/signup" element={<SignUp/>} />
 <  Route path="/dashboard/about" element={<About/>} />
