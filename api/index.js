e3eecc36b8a1570bc16ecd58513f417d6623dd67
mJawed <mohd.jawed@alcanttor.com>
2024-01-02 12:31:16 +0530

    Database connected with app

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
-
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express()
 
 app.listen(3000, ()=>{
 console.log('server is running on port 3000!!')
 });
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
