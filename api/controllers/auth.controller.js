5aed8c042123c26bc3fdf86d74ef30cb88c3c0d2
mJawed <mohd.jawed@alcanttor.com>
2024-01-03 18:22:17 +0530

    created a middleware and a fuction to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import  bcrypt from "bcryptjs"
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 
 const saltRounds = 10; 
@@ -11,6 +12,16 @@ try {
     const passwordHash = await bcrypt.hash(password,saltRounds)
     console.log(req.body );
     console.log(passwordHash)
+
+    if(!username 
+    || !email 
+    || !password  
+    || username === '' 
+    || email === '' 
+    || password === ''){
+
+    next(errorHandler(400,"All fields are required"))
+    }
 
    const newuser  = new User({
         username,
@@ -26,12 +37,14 @@ res.status(201).json({ message: "User created successfully" });
     
   } catch (error) {
     console.log(error.name)
-    if (error.name === 'ValidationError') {
+/*     if (error.name === 'ValidationError') {
         const errorMessages = Object.values(error.errors).map(err => err.message);
         res.status(400).json({ errors: errorMessages });
+        next(error)
       } else {
         res.status(500).json({ message: "An error occurred during sign-up" });
-      }
-
+        next(error)
+      } */
+      next(error)
   }
 }
--- api/index.js
+++ api/index.js
@@ -36,3 +36,17 @@ mongoose
 
   app.use('/api/user', userRoutes);
   app.use('/api/auth', authRoutes);
+
+  //Middleware
+
+  app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    res.status(statusCode).json({
+      success : false,
+      statusCode,
+      message
+    })
+
+  })
+  
